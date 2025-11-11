c8de03765eaea92d1627d423d81bd8c5bf052168
Albiere123 <albieregames30@gmail.com>
2025-11-11 09:16:57 -0300

    Versão 0.0.1

---
+++ src/main/java/br/ufpb/dcx/lima/albiere/TesteAlbiere.java
@@ -0,0 +1,9 @@
+package br.ufpb.dcx.lima.albiere;
+
+public class TesteAlbiere {
+    void main() {
+        IO.println("Olá Mundo!");
+        String name = IO.readln("Qual é o seu nome? \n");
+        IO.println("Olá, " + name);
+    }
+}
